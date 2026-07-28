5cbdcedb7b135634dab2a1678fd817cfee43a836
Fernando Campos <fcampos081981@hotmail.com>
2026-07-28 15:12:04 -0300

    chore: properties

--- src/main/java/com/cleanarch/wishlist/WishlistApplication.java
+++ src/main/java/com/cleanarch/wishlist/WishlistApplication.java
@@ -2,6 +2,11 @@ package com.cleanarch.wishlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@EnableMongoRepositories(basePackages = "com.cleanarch.wishlist.infrastructure.repository.mongo")
+@EnableJpaRepositories(basePackages = "com.cleanarch.wishlist.infrastructure.repository.postgresql")
 
 @SpringBootApplication
 public class WishlistApplication {
